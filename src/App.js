551f2399f7e7993b6562d085129a566acb20b5f0
Michael Chan <michaelcychan@gmail.com>
2022-07-29 12:29:12 +0100

    booking button and simple counter

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ import { useState } from "react";
 function App() {
   const doctorStatuses = ["IN", "OUT"];
   const [doctorStatus, setDoctorStatus] = useState("OUT");
+  const [waitingPatientTotalNumber, setWaitingPatientTotalNumber] = useState(0);
+  const [waitingPatientTodayNumber, setWaitingPatientTodayNumber] = useState(0);
+  const [waitingPatientTomorrowNumber, setWaitingPatientTomorrowNumber] = useState(0);
+
+  const registerBookingToday = () => {
+    setWaitingPatientTodayNumber(waitingPatientTodayNumber + 1);
+    setWaitingPatientTotalNumber(waitingPatientTotalNumber + 1);
+  }
+
+  const regsiterBookingTomorrow = () => {
+    setWaitingPatientTomorrowNumber(waitingPatientTomorrowNumber + 1);
+    setWaitingPatientTotalNumber(waitingPatientTotalNumber + 1);
+  }
 
   return (
     <div className="App">
@@ -17,11 +30,17 @@ function App() {
       </header>
         <div className="Clinic-open-status">
           <h1 className="doctor-in-or-out">The Doctor is {doctorStatus} !</h1>
-          
+          <h2>There are {waitingPatientTotalNumber} bookings in total.</h2>
+          <h3>There are {waitingPatientTodayNumber} bookings for today.</h3>
+          <h3>There are {waitingPatientTomorrowNumber} bookings for today.</h3>
+          <button onClick={registerBookingToday}>Book for today</button>
+          <button onClick={regsiterBookingTomorrow}>Book for tomorrow</button>
+        </div>
+        <div className="doctor-use">
           {doctorStatuses.map((status) => (
             <DoctorInOutButton text={status} setStatus={setDoctorStatus} />
           ))}
-        </div>
+          </div>
     </div>
   );
 }
